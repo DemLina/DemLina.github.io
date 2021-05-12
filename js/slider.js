d358eb36f92a9c969dd3327270920ec8c8352d51
Lina <44467521+DemLina@users.noreply.github.com>
2021-05-12 19:52:57 +0300

    create button page-up

--- js/slider.js
+++ js/slider.js
@@ -27,4 +27,11 @@ $('.slider__wrapp').slick({
     ]
   });
 
-
+    $(function() {
+    
+      $('#up').click(function() {
+        $('html, body').animate({scrollTop: 0},500);
+        return false;
+      })
+    
+    })
