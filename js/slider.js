9ecf91d9273d67a921aef34994fab75bb51076ca
Lina <44467521+DemLina@users.noreply.github.com>
2021-05-12 18:58:52 +0300

    init commit

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+
+$('.slider__wrapp').slick({
+    slidesToShow: 3,
+    prevArrow: $(".slider__arrow-prev"),
+    nextArrow: $(".slider__arrow-next"),
+    responsive: [
+	    {
+	      breakpoint: 1280,
+	      settings: {
+	        slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: false,
+          initialSlide: 1,
+
+	      }
+	    },
+	    {
+	      breakpoint: 767,
+	      settings: {
+	        slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 0,
+            centerMode: false,
+            dots: false,
+	      }
+	    }
+    ]
+  });
+
+
